c27d964594c35fc966a39fc0bba636fab09b07ac
Labinot77 <156574729+Labinot77@users.noreply.github.com>
2024-07-22 16:11:18 +0300

    New exercises

--- While Loop - Exercise/06. Cake.js
+++ While Loop - Exercise/06. Cake.js
@@ -1,3 +1,38 @@
 function cake(params) {
+  const length = Number(params[0]);
+  const width = Number(params[1]);
+  const totalPieces = length * width
+  let remaningPieces = totalPieces
+  let index = 2;
+
   
+  while (index < params.length ) {
+    let command = params[index];
+
+    if (command === "STOP") {
+      console.log(`${remaningPieces} pieces are left.`);
+      return;
+    }
+    
+    let piecesTaken = Number(params[index]);
+    remaningPieces -= piecesTaken;
+
+    if (remaningPieces < 0) {
+      console.log(`No more cake left! You need ${Math.abs(remaningPieces)} pieces more.`)
+      return;
+    }
+
+    index++
+  } 
+
+  console.log(`${remaningPieces} pieces are left.`);
 }
+
+cake(["10",
+  "10",
+  "20",
+  "20",
+  "20",
+  "20",
+  "21"])
+  
